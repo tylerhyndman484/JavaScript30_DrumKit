b1e3e04dc2da47dedafb1ed922f0d9f227864d2c
Tyler Hyndman <tylerhyndman484@hotmail.com>
2024-10-17 16:59:08 -0500

    breaking down solution to understand it

--- script.js
+++ script.js
@@ -2,14 +2,9 @@
 // Make it known visually what key you are pressing down
 // Make the sound stop for current audio file when press down another key
 
+function playAudio(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    audio.play();
+}
 
-function playAudio() {
-    window.addEventListener('keydown', (key) => {
-        let aKey = document.getElementById('A');
-        if (key === 'a') {
-            aKey.play();
-        }
-    }
-)}
-
-playAudio();
+window.addEventListener('keydown', playAudio);
